686c3d646e1a589203c9ee2565debedc3ddfb5d5
donsiu <spcdonsiu@gmail.com>
2015-05-13 07:16:58 -0500

    Unity Game Rolling balls

---
+++ Assets/script/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float speed;
+
+	public Text countext;
+	public Text wintext;
+	private Rigidbody rb;
+
+	public int count;
+
+	void SetcountText()
+	{
+		countext.text = "Score:" + count.ToString();
+		if (count >= 9)
+		{
+			wintext.text = "You Win";
+
+		}
+	}
+	
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		 count = 0;
+		SetcountText();
+		wintext.text = "";
+
+	}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+//		Debug.LogWarning (moveHorizontal);
+//		Debug.LogWarning (moveVertical);
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		rb.AddForce (movement * speed);
+	}
+
+	void  OnTriggerEnter(Collider collid)
+	{
+		if (collid.gameObject.CompareTag ("Pick Up")) {
+			collid.gameObject.SetActive(false);
+			count ++;
+			SetcountText();
+		}
+
+		//Destroy (other.gameObject);
+
+	}
+}
---
+++ Assets/script/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 v3 = new Vector3 (15,30,45);
+		transform.Rotate (v3 * Time.deltaTime);
+	}
+}
